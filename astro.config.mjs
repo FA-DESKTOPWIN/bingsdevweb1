481254ecb460ef186b81ec0de5e6f8523e6c1d8e
FA-DESKTOPWIN <214901886+FA-DESKTOPWIN@users.noreply.github.com>
2026-07-02 10:28:58 -0300

    Configure base path for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  base: '/bingsdevweb1/',
   integrations: [
     tailwind({
       applyBaseStyles: false,
